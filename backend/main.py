09c5948d15901ec9c258d74a1eda4bf01f32fbc7
Ivan Stoychev <IYStoychev21@codingburgas.bg>
2025-07-01 17:45:01 +0300

    fix(backend): add frontend url to CORS

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(auth_middleware.AuthMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://gentle-tree-0f9312803.2.azurestaticapps.net"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
